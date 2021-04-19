94492bb8e7e07eca1454ebaea9653b7915302c1c
Felipe Hernandez <72527112+juanfelipehdezm@users.noreply.github.com>
2021-04-19 10:30:10 -0500

    SQL exercises

--- SQL/answers.sql
+++ SQL/answers.sql
@@ -51,4 +51,33 @@ SELECT DISTINCT CITY
 FROM STATION 
 WHERE LEFT(CITY,1) IN ('a','e','i','o','u')
 AND RIGHT(CITY,1) IN ('a','e','i','o','u')
-ORDER BY CITY
+ORDER BY CITY;
+
+/*Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.*/
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u');
+
+/*Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.*/
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name,3),ID asc;
+
+/*Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements 
+or each record in the table:
+
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 3 sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+Values in the tuple (13,14,30) cannot form a triangle because the combined value of sides A and B is not larger than that of side .*/
+SELECT CASE 
+           WHEN (A+B) <= C OR (A+C) <= B OR (B+C) <= A THEN 'Not A Triangle'
+           WHEN A = B AND B = C THEN 'Equilateral'
+           WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+           ELSE 'Scalene'
+       END AS type_of_triangule
+FROM TRIANGLES;
