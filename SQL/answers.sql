8621c2a46a05a49cdf27d3ab5a3f802bb663d84a
Felipe Hernandez <72527112+juanfelipehdezm@users.noreply.github.com>
2021-05-10 11:50:20 -0500

    New Companies

--- SQL/answers.sql
+++ SQL/answers.sql
@@ -111,3 +111,14 @@ WHERE (months*salary) =
     FROM Employee
     )
 GROUP BY earnings;
+
+/*https://www.hackerrank.com/challenges/the-company/problem?isFullScreen=true    SQL server*/
+SELECT 
+    c.company_code,c.founder,COUNT(DISTINCT e.lead_manager_code),
+    COUNT(DISTINCT e.senior_manager_code),COUNT(DISTINCT e.manager_code),
+    COUNT(DISTINCT e.employee_code)
+FROM Company AS c
+INNER JOIN Employee AS e
+ON c.company_code = e.company_code
+GROUP BY c.company_code,c.founder
+ORDER BY c.company_code;
